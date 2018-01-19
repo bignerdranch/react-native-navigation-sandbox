2910fe410c6d68031fa4f55e231622f2fdd87f94
Josh Justice <jjustice@bignerdranch.com>
2018-01-19 14:06:47 -0500

    Remove tabs

--- index.js
+++ index.js
@@ -5,21 +5,11 @@ import { registerScreens } from './screens';
 registerScreens(); // this is where you register all of your app's screens
 
 // start the app
-Navigation.startTabBasedApp({
-  tabs: [
-    {
-      label: 'One',
-      screen: 'example.FirstTabScreen', // this is a registered name for a screen
-      // icon: require('../img/one.png'),
-      // selectedIcon: require('../img/one_selected.png'), // iOS only
-      title: 'Screen One'
-    },
-    {
-      label: 'Two',
-      screen: 'example.SecondTabScreen',
-      // icon: require('../img/two.png'),
-      // selectedIcon: require('../img/two_selected.png'), // iOS only
-      title: 'Screen Two'
-    }
-  ]
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: 'example.FirstTabScreen', // this is a registered name for a screen
+    // icon: require('../img/one.png'),
+    // selectedIcon: require('../img/one_selected.png'), // iOS only
+    title: 'Screen One'
+  }
 });
